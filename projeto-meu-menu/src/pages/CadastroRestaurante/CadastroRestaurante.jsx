9a8e04989902c6f438e30ef6c17b5943e3bc761a
kauan-mendes <kauan.mendes@sptech.school>
2023-04-25 14:23:37 -0300

    Andamento na tela de cadastro do restaurante usem como exemplo

--- projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
+++ projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
@@ -5,11 +5,12 @@ import { Col, Container, Row } from "react-bootstrap"
 export default function CadastroRestaurante() {
   return (
     <Container fluid>
-      <Row>
-        <Col>
-        <img src="/assets/images/logoBranco.svg" alt="" />
+      <Row className="d-flex flex-wrap">
+        <Col lg={7} md={12} className="container-image d-none d-lg-flex flex-column">
+          <img src="/assets/images/logoBranco.svg" alt="logo meu menu" />
+          <img src="/assets/images/chefeCadastro.svg" alt="Aspas de operator" />
         </Col>
-        <Col>
+        <Col lg={5} md={12} className="container-form">
 
         </Col>
       </Row>
