40ab67d41719fc3708a6aae6c15f61a1a67492bd
kauan-mendes <kauan.mendes@sptech.school>
2023-04-25 18:26:31 -0300

    Andamento tela de cadastro restaurante usem de referencia

--- projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
+++ projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import './style.css'
-import { Col, Container, Row } from "react-bootstrap"
+import { Button, Col, Container, Form, Row } from "react-bootstrap"
 
 export default function CadastroRestaurante() {
   return (
     <Container fluid>
-      <Row className="d-flex flex-wrap">
-        <Col lg={7} md={12} className="container-image d-none d-lg-flex flex-column">
-          <img src="/assets/images/logoBranco.svg" alt="logo meu menu" />
-          <img src="/assets/images/chefeCadastro.svg" alt="Aspas de operator" />
+      <Row className="d-flex flex-wrap ">
+        <Col lg={7} md={7} sm={7} className="container-image d-none d-lg-flex flex-column">
+          <img className="imgLogo" src="/assets/images/logoBranco.svg" alt="logo meu menu" />
+          <img className="imgChefe" src="/assets/images/chefeCadastro.svg" alt="Aspas de operator" />
         </Col>
-        <Col lg={5} md={12} className="container-form">
+
+        <Col lg={5} md={5} sm={5} className="container-form d-none d-lg-flex flex-column">
+
+          <h1 className="passos">PASSO <b className="passoAtual"> 01</b> <span className="passosFaltantes"> 02 03 </span></h1>
+          <p>Todos os campos são obrigatórios</p>
+          <Form>
+            <Form.Control className="mb-3" type="email" placeholder="Nome Fantasia" />
+            <Form.Control className="mb-3" type="number" placeholder="CNPJ ou CPF" />
+            <Button className="buttonAvancar">Avançar</Button>
+          </Form>
 
         </Col>
       </Row>
-    </Container>
+    </Container >
   )
 }
