9800794eb60c6f346a623ea9a984e20fd39d7b5c
kauan-mendes <kauan.mendes@sptech.school>
2023-04-26 16:55:04 -0300

    push

--- projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
+++ projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import './style.css'
 import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import { BiArrowBack } from 'react-icons/bi';
+
 
 export default function CadastroRestaurante() {
   return (
     <Container fluid>
       <Row className="d-flex flex-wrap ">
-        <Col lg={7} md={7} sm={7} className="container-image d-none d-lg-flex flex-column">
+        <Col lg={7} md={6} className="container-image d-none d-md-flex flex-column">
+        <BiArrowBack className="align-self-start mt-5 " size="80px" fill="#ffffff"/>
           <img className="imgLogo" src="/assets/images/logoBranco.svg" alt="logo meu menu" />
           <img className="imgChefe" src="/assets/images/chefeCadastro.svg" alt="Aspas de operator" />
         </Col>
+        
+        <Col lg={5} md={6} sm={12} className="container-form d-flex flex-column">
+        <BiArrowBack className="align-self-start mb-5 d-flex d-md-none" size="80px"/>
+          <Col lg={10} md={12}>
+            <div className="container-header w-100 mb-5 d-flex flex-column ">
+              <div className="steps d-flex flex-wrap justify-content-center gap-3">
+                <h1 className=""> PASSO </h1>
+                <h1 className=""> <b className="passoAtual"> 01</b></h1>
+                <h1 className=""> <span className="passosFaltantes"> 02 </span></h1>
+                <h1 className=""> <span className="passosFaltantes"> 03 </span></h1>
+              </div>
+              <p className="form-text-info">Todos os campos são obrigatórios</p>
+            </div>
 
-        <Col lg={5} md={5} sm={5} className="container-form d-none d-lg-flex flex-column">
-
-          <h1 className="passos">PASSO <b className="passoAtual"> 01</b> <span className="passosFaltantes"> 02 03 </span></h1>
-          <p>Todos os campos são obrigatórios</p>
-          <Form>
-            <Form.Control className="mb-3" type="email" placeholder="Nome Fantasia" />
-            <Form.Control className="mb-3" type="number" placeholder="CNPJ ou CPF" />
-            <Button className="buttonAvancar">Avançar</Button>
-          </Form>
-
+            <Form className="form-content w-100 d-flex flex-column justify-content-end align-items-end">
+              <Form.Control size="lg" className="input-form w-100" type="email" placeholder="Nome Fantasia" />
+              <Form.Control size="lg" className="input-form w-100" type="number" placeholder="CNPJ ou CPF" />
+              <Button className="buttonAvancar">Avançar</Button>
+            </Form>
+          </Col>
         </Col>
       </Row>
     </Container >
--- projeto-meu-menu/src/pages/Home/Home.jsx
+++ projeto-meu-menu/src/pages/Home/Home.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 // import '../assets/css/arquivo.css'
+import { Button, Carousel, Col, Container, Form, Row } from "react-bootstrap"
 export default function Home(){
     
 
     return(
 
         <>
-        <p>OI home aq</p>
+       <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/assets/images/chefeCadastro.svg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/assets/images/chefeCadastro.svg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/assets/images/chefeCadastro.svg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
         </>
     )
 
