2c141c58f734d1f4ae5fe59b6e8f542c5e3c1a98
kauan-mendes <kauan.mendes@sptech.school>
2023-04-26 16:56:00 -0300

    pega ai bruno

--- projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
+++ projeto-meu-menu/src/pages/CadastroRestaurante/CadastroRestaurante.jsx
@@ -9,13 +9,13 @@ export default function CadastroRestaurante() {
     <Container fluid>
       <Row className="d-flex flex-wrap ">
         <Col lg={7} md={6} className="container-image d-none d-md-flex flex-column">
-        <BiArrowBack className="align-self-start mt-5 " size="80px" fill="#ffffff"/>
+          <BiArrowBack className="align-self-start mt-5 " size="80px" fill="#ffffff" />
           <img className="imgLogo" src="/assets/images/logoBranco.svg" alt="logo meu menu" />
           <img className="imgChefe" src="/assets/images/chefeCadastro.svg" alt="Aspas de operator" />
         </Col>
-        
+
         <Col lg={5} md={6} sm={12} className="container-form d-flex flex-column">
-        <BiArrowBack className="align-self-start mb-5 d-flex d-md-none" size="80px"/>
+          <BiArrowBack className="align-self-start mb-5 d-flex d-md-none" size="80px" />
           <Col lg={10} md={12}>
             <div className="container-header w-100 mb-5 d-flex flex-column ">
               <div className="steps d-flex flex-wrap justify-content-center gap-3">
