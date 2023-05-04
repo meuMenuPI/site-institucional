158009d696195e69fd3a61ca1346679b966d609b
BrunoCara <bruno.cara@sptech.school>
2023-05-04 14:18:24 -0300

    inputs

--- projeto-meu-menu/src/pages/cadastro-usuario/components/inputs.jsx
+++ projeto-meu-menu/src/pages/cadastro-usuario/components/inputs.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import { BiArrowBack } from 'react-icons/bi';
 
 function Inputs(){
     return (
-        <input className="inputs">
-            
-        </input>
+        <Form className="form-content w-100">
+        <Form.Control size="lg" className="input_cad_usuario" type="name" placeholder="Nome" />
+        <Form.Control size="lg" className="input_cad_usuario" type="name" placeholder="Sobrenome" />
+        <Form.Control size="lg" className="input_cad_usuario" type="number" placeholder="CPF" />
+        <Button className="btn_cad_usuario">Avançar</Button>
+        </Form>
     );
 }
 
