1f63628f9141b61dd1cd15fc24303186ec019e0a
lsoaress <lucassoaresdecarvalho2013@gmail.com>
2023-05-18 20:33:11 -0300

    api na parte cardapio

--- projeto-meu-menu/src/components/restaurantePaginaComponents/ItemCardapio.jsx
+++ projeto-meu-menu/src/components/restaurantePaginaComponents/ItemCardapio.jsx
@@ -1,22 +1,48 @@
 import React, { useState } from 'react'
 import Design from '../../assets/images/design_cadastro_usuario.png'
 
+import { useEffect } from 'react';
+import api from '../../api'
+
 function ItemCardapio(props) {
+
+    const [cardapio, setCardapio] = useState([]);
+
+    /* sessionStorage.ID_RESTAURANTE_CARDAPIO */
+    var id = 1;
+
+    useEffect(() => {
+        api.get(`/cardapios?id=${id}`)
+            .then((respostaObtida) => {
+                setCardapio(respostaObtida.data);
+                console.log(respostaObtida.data)
+            })
+            .catch((erroObtido) => {
+                console.log(erroObtido);
+            });
+    }, []);
+
     return (
-        <div className='rp_itemCardapio'>
-            <div className='rp_alinharCardapio'>
-                    <div className='rp_descricaoPrato'>
-                        <span className='rp_nomePrato'>{props.nome}</span>
-                        {props.descricao}
+        <>
+            {cardapio.map((item) =>
+
+                <div key={item.id} className='rp_itemCardapio'>
+                    <div className='rp_alinharCardapio'>
+                        <div className='rp_descricaoPrato'>
+                            <span className='rp_nomePrato'>{item.nome}</span>
+                            {item.descricao}
+                        </div>
+                        <div className='rp_precoPrato'>
+                            R${item.preco}
+                        </div>
+                        <div className='rp_fotoPrato'>
+                            <img src={Design} alt="" className='rp_fotoPrato' />
+                        </div>
                     </div>
-                    <div className='rp_precoPrato'>
-                        R${props.preco}
-                    </div>
-                    <div className='rp_fotoPrato'>
-                        <img src={props.img} alt="" className='rp_fotoPrato'/>
-                    </div>
-            </div>
-        </div>
+                </div>
+            )}
+        </>
+
     )
 }
 
--- projeto-meu-menu/src/components/restaurantePaginaComponents/ModificadorConteudo.jsx
+++ projeto-meu-menu/src/components/restaurantePaginaComponents/ModificadorConteudo.jsx
@@ -3,7 +3,6 @@ import Cardapio from './CardapioPagina';
 import ItemCardapio from './ItemCardapio';
 import ReviewRestaurante from './ReviewRestaurante'
 import FotoCardapio from './FotoCardapio';
-import Design from '../../assets/images/design_cadastro_usuario.png'
 import SelectReview from '../../components/restaurantePaginaComponents/SelectReview';
 
 // import { Container } from './styles';
@@ -14,7 +13,7 @@ function ModificadorConteudo(props) {
         return (
             <>
                 <Cardapio />
-                <ItemCardapio nome="Teste nome do prato" descricao="Exemplo da descricao de um prato" preco="10.0" img={Design} />
+                <ItemCardapio/>
             </>
         )
     }
