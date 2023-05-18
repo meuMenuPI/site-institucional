bb6b36eb8429d97b0e5497e5df7f202923b1220d
Bruno Lima <Bruno.Lima1@br.ey.com>
2023-05-18 18:20:40 -0300

    review do restaurante finalizada

--- projeto-meu-menu/src/components/restaurantePaginaComponents/ReviewRestaurante.jsx
+++ projeto-meu-menu/src/components/restaurantePaginaComponents/ReviewRestaurante.jsx
@@ -1,8 +1,46 @@
 import React, { useState } from 'react'
-
+import SelectReview from './SelectReview';
 function ReviewRestaurante(props) {
+    const dados = [{
+        nome: 'bruno',
+        foto: 'https://tse4.mm.bing.net/th/id/OIP.WdCnDnwGRrv1AQWR_VKF4wHaII?pid=ImgDet&rs=1',
+        data: '18/05/2022 - 18:11',
+        estrelas: 5,
+        descricao: 'Review Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos facere fugit dolor, officiis suscipit quaerat porro temporibus aspernatur molestiae ratione nihil alias sit, eaque culpa laboriosam! Saepe quos illo deserunt!'
+    },{
+        nome: 'bruno',
+        foto: 'https://tse4.mm.bing.net/th/id/OIP.WdCnDnwGRrv1AQWR_VKF4wHaII?pid=ImgDet&rs=1',
+        data: '18/05/2022 - 18:11',
+        estrelas: 5,
+        descricao: 'Review Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos facere fugit dolor, officiis suscipit quaerat porro temporibus aspernatur molestiae ratione nihil alias sit, eaque culpa laboriosam! Saepe quos illo deserunt!'
+    },
+    {
+        nome: 'Bruno Cara',
+        foto: 'https://tse4.mm.bing.net/th/id/OIP.WdCnDnwGRrv1AQWR_VKF4wHaII?pid=ImgDet&rs=1',
+        data: '18/05/2022 - 18:11',
+        estrelas: 5,
+        descricao: 'Review Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos facere fugit dolor, officiis suscipit quaerat porro temporibus aspernatur molestiae ratione nihil alias sit, eaque culpa laboriosam! Saepe quos illo deserunt!'
+    }]
     return (
-        <></>
+        <>  
+            {dados.map((dadostemp) => 
+                            <div className='rp_containerReview'>
+                            <div className='rp_alinharReview'>
+                                <div className='rp_divFotoReview'>
+                                    <div className='rp_fotoReview' style={{ backgroundImage: `url(${dadostemp.foto})` }}></div>
+                                </div>
+                                <div className='rp_infoReview'>
+                                    <div className='rp_divNomeReview'>
+                                        <span className='rp_nomeReview'>{dadostemp.nome}</span>
+                                        <span className='rp_dataReview'>{dadostemp.data}</span>
+                                    </div>
+                                    <div>{dadostemp.estrelas}</div>
+                                    <div className='rp_textoReview'>{dadostemp.descricao}</div>
+                                </div>
+                            </div>
+                        </div>
+            )}
+        </>
     )
 }
 
--- projeto-meu-menu/src/pages/RestaurantePagina/RestaurantePagina.jsx
+++ projeto-meu-menu/src/pages/RestaurantePagina/RestaurantePagina.jsx
@@ -6,18 +6,20 @@ import Cardapio from '../../components/restaurantePaginaComponents/CardapioPagin
 import ItemCardapio from '../../components/restaurantePaginaComponents/ItemCardapio';
 import Design from '../../assets/images/design_cadastro_usuario.png'
 import FotoCardapio from '../../components/restaurantePaginaComponents/FotoPagina';
-
+import ReviewRestaurante from '../../components/restaurantePaginaComponents/ReviewRestaurante';
+import SelectReview from '../../components/restaurantePaginaComponents/SelectReview';
 function RestaurantePagina() {
   return (
     <>
       <HeaderPagina imgFundo={Logo_preto}/>
       <MenuPagina />
-      <Cardapio>
+      {/* <Cardapio>
       </Cardapio>
       <ItemCardapio nome="Teste nome do prato" descricao="Exemplo da descricao de um prato" preco="10.0" img={Design}/>
       <ItemCardapio nome="Teste nome do prato" descricao="Exemplo da descricao de um prato exemplo da descricao de um prato " preco="10.0" img={Design}/>
-      <ItemCardapio nome="Teste nome do prato" descricao="Exemplo da descricao de um prato" preco="10.0" img={Design}/> 
-  
+      <ItemCardapio nome="Teste nome do prato" descricao="Exemplo da descricao de um prato" preco="10.0" img={Design}/>  */}
+      <SelectReview></SelectReview>
+      <ReviewRestaurante imgFundo={Logo_preto}/>
       </>
   )
 }
