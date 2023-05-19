242754136f827881dfe1b8b512c50977860438a6
kauan-mendes <kauan.mendes@sptech.school>
2023-05-18 21:00:12 -0300

    Acrescenta validação na cadatro Usuario p2

--- projeto-meu-menu/src/components/cadastroUsuario/inputs.jsx
+++ projeto-meu-menu/src/components/cadastroUsuario/inputs.jsx
@@ -6,7 +6,7 @@ function Inputs(props){
     const [nome, setNome] = useState(props.nome);
 
     return (
-        <Form.Control size="lg" className="input_cad_usuario" type="name" placeholder={props.placeholder} onChange={(e) => setNome(e.target.value)} name={props.name} style={{backgroundColor: props.color}}/>
+        <Form.Control size="lg" className="i_c_d" type="name" placeholder={props.placeholder} onChange={(e) => setNome(e.target.value)} name={props.name} style={{backgroundColor: props.color}}/>
     );
 }
 
