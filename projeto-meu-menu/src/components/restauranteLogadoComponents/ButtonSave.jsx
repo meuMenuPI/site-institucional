03dda17fe3125c88ee84c5850072ac5c237c859c
Rafael-Caxixi <rafael.fuzeti@sptech.school>
2023-05-27 02:03:24 -0300

    Está atualizando no banco mas com valores definidos

--- projeto-meu-menu/src/components/restauranteLogadoComponents/ButtonSave.jsx
+++ projeto-meu-menu/src/components/restauranteLogadoComponents/ButtonSave.jsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { useState } from "react";
+import api from "../../api";
+import { useNavigate } from 'react-router-dom';
+
+const formTemplate = {
+  nome: "Marcelo's Bar",
+  especialidade: 'CHINESA',
+  telefone: "11999999999",
+  site: "https://www.you.com/",
+  estrela: 5
+};
 
 function ButtonSave(props) {
+  const [data, setData] = useState(formTemplate);
+  const navigate = useNavigate();
+
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  function atualizar(e) {
+    e.preventDefault();
+
+    const restauranteInfo = {
+      usuario: 1,
+      id: 1,
+      nome: "Marcelo's Bar",
+      especialidade: 'CHINESA',
+      telefone: "11999999999",
+      site: "https://www.you.com/",
+      estrela: 5
+    };
+
+    api
+      .put(`/restaurantes/${restauranteInfo.id}`, restauranteInfo)
+      .then((res) => {
+        console.log(res.data);
+
+        alert("Atualizado com sucesso!");
+      })
+      .catch((erro) => {
+        console.error(erro);
+        alert("Não foi possível atualizar o restaurante, tente novamente.");
+        navigate("/restaurante-perfil");
+      });
+  }
+
   return (
-
     <>
-
       <div className="div_button_save">
-        <button id='id_button_save'>{props.text}</button>
+        <button onClick={atualizar} id='id_button_save'>{props.text}</button>
       </div>
-
     </>
-
-  )
+  );
 }
 
-export default ButtonSave
+export default ButtonSave;
--- projeto-meu-menu/src/pages/RestauranteLogado/RestaurantePerfil.jsx
+++ projeto-meu-menu/src/pages/RestauranteLogado/RestaurantePerfil.jsx
@@ -1,19 +1,65 @@
-import React from 'react'
-import MenuLeft from '../../components/restauranteLogadoComponents/MenuLeft'
-import SideEditionProfile from '../../components/restauranteLogadoComponents/SideEditionProfile'
-import './style.css'
+import React from 'react';
+import MenuLeft from '../../components/restauranteLogadoComponents/MenuLeft';
+import SideEditionProfile from '../../components/restauranteLogadoComponents/SideEditionProfile';
+import './style.css';
+import { useState } from "react";
+import api from "../../api";
+import { useNavigate } from 'react-router-dom';
+
+const formTemplate = {
+    nomeRestaurante: "Pedro",
+    telefone: "11951676901",
+    estrela: 3,
+    site: "https://www.youtubeaaa.com/"
+};
+/* 
+const formTemplate2 = {
+    cep: "",
+    numero: "",
+    complemento: ""
+}; */
 
 function RestaurantePerfil() {
+    const [data, setData] = useState(formTemplate);
+    const navigate = useNavigate();
+
+    const updateFieldHandler = (key, value) => {
+        setData((prev) => ({ ...prev, [key]: value }));
+    };
+
+    /*  function atualizar(e) {
+         e.preventDefault();
+ 
+         const restauranteInfo = {
+             usuario: 1,
+             id: 1,
+             nomeRestaurante: data.nomeRestaurante,
+             telefone: data.telefone,
+             estrela: data.estrela,
+             site: data.site
+         };
+ 
+         api
+             .put(`/restaurantes/${restauranteInfo.id}`, restauranteInfo)
+             .then((res) => {
+                 console.log(res.data);
+                 alert("Atualizado com sucesso!");
+             })
+             .catch((erro) => {
+                 console.error(erro);
+                 alert("Não foi possível atualizar o restaurante, tente novamente.");
+                 navigate("/restaurante-perfil");
+             });
+     } */
+
     return (
         <>
-        <div className="all">
-            <MenuLeft />
-             <SideEditionProfile /> 
-        </div>
-            
-
+            <div className="all">
+                <MenuLeft />
+                <SideEditionProfile />
+            </div>
         </>
-    )
-};
+    );
+}
 
 export default RestaurantePerfil;
