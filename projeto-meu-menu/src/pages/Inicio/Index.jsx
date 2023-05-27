794ebed2ecbe379b6ca7bda3dbed59a079c0ea53
breno-galante <breno.galante@sptech.school>
2023-05-27 15:38:07 -0300

    Finalização index + Atualização da tela de perfil

--- projeto-meu-menu/src/pages/Inicio/Index.jsx
+++ projeto-meu-menu/src/pages/Inicio/Index.jsx
@@ -83,24 +83,42 @@ export default function Index() {
                 capa3={LaGuapa} nomeRestaurante3="La Guapa" />
               <Restaurante titulo="Descubra novas culturas" capa={Kfc} nomeRestaurante="KFC"
                 capa2={DivinoFogao} nomeRestaurante2="Divino Fogão"
-                capa3={LaGuapa} nomeRestaurante3="La Guapa" /> *
+                capa3={LaGuapa} nomeRestaurante3="La Guapa" />
             </div>
           </div>
         </div>
       </Col>
       <Col className="sobre">
-        <div className='fundoFood'>
+        <div className='fundoFood d-flex justify-content-between align-itens-center'>
+
+          <div className='caixaEsquerda d-flex flex-column'>
+            <h3>Conheça seu</h3>
+            <h1>Novo Serviço de Localização</h1>
+          </div>
+
+          <div className='caixaDireita'>
+            <div className='texto d-flex flex-column justify-content-between'>
+              <div>Meu Menu é uma startup, criada em 2022, que visa suprir as dores dos comerciantes e usuários que buscam o melhor estabelecimento para realizar suas refeições.</div>
+
+              <div>Nossos comercios parceiros realizam os melhores atendimentos de todo território nacional. Hoje o Meu Menu indica mais de mil comercios  ao mês, otimizando o custo e o ambiente selecionado. </div>
+
+
+              <div>Seja usuário ou comerciante,</div>
+              <div><h3>O Meu Menu é para você!</h3></div>
+            </div>
+          </div>
 
         </div>
+        <Row className="rodape d-flex flex-column justify-content-around align-items-center">
+          <div className='w-100 h-70 d-flex'>
+            <img className='imgLogo' src={Logo} alt="Logo meu menu" />
+          </div>
+          <div className='direitoAutoral d-flex justify-content-center'>
+            <h2 className='align-self-center'>© Meu Menu 2023</h2>
+          </div>
+        </Row>
       </Col>
-      <Row className="rodape m-0 d-flex flex-column justify-content-around align-items-center">
-        <div className='w-100 h-70 d-flex'>
-          <img className='imgLogo' src={Logo} alt="Logo meu menu" />
-        </div>
-        <div className='direitoAutoral d-flex justify-content-center'>
-          <h2 className='align-self-center'>© Meu Menu 2023</h2>
-        </div>
-      </Row>
+
     </Container>
 
 
