393871664fd8a93b7feeb8c7edab7ea2c40eceea
BrunoCara <bruno.cara@sptech.school>
2023-05-31 12:17:46 -0300

    barra de pesquisa funcional

--- projeto-meu-menu/src/components/inicioComponents/BarraBusca.jsx
+++ projeto-meu-menu/src/components/inicioComponents/BarraBusca.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../pages/Inicio/style.css'
 import IconeLupa from '../../assets/images/iconeLupa.svg'
+import api from '../../api'
+import ResultadoBusca from './ResultadoBusca'
 
 const BarraBusca = (props) => {
+
+    const [data, setData] = useState([]);
+    const [busca, setBusca] = useState('');
+    
+    const pesquisa = (e) => {
+        e.preventDefault();
+        const {value} = e.target;
+        setBusca(value);
+
+        if(!value) {
+           setData([])
+           return;
+        }
+
+            api.get("/restaurantes")
+              .then((respostaObtida) => {
+                console.log(respostaObtida.data);
+                setData(respostaObtida.data)
+              })
+              .catch((erroObtido) => {
+                console.log(erroObtido)
+              });
+  
+    }
+
     return (
         <>
-                
-                <input className='barraBusca' placeholder='Busque o que deseja' />
-                <img src={IconeLupa}className='iconeLupa' />
+            <div className='containerBusca'>
+                <form>
+                    <input id='busca' name='busca' className='barraBusca' placeholder='Busque o que deseja' onChange={pesquisa}/>
+                    <img src={IconeLupa} className='iconeLupa' alt=''/>
+                </form>
+                <ResultadoBusca data={data} busca={busca}></ResultadoBusca>
+            </div>
         </>
     )
 }
 
 export default BarraBusca
+
