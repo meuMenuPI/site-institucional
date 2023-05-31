c9e8cbae3ed53ad4cf2ed3ebe2c944c6a70316e8
lsoaress <lucassoaresdecarvalho2013@gmail.com>
2023-05-31 17:02:33 -0300

    reset de um commit

--- projeto-meu-menu/src/pages/RestauranteLogado/RestaurantePerfil.jsx
+++ projeto-meu-menu/src/pages/RestauranteLogado/RestaurantePerfil.jsx
@@ -1,53 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuLeft from '../../components/restauranteLogadoComponents/MenuLeft';
 import SideEditionProfile from '../../components/restauranteLogadoComponents/SideEditionProfile';
 import './style.css';
-/* import { useState } from "react"; */
+import api from '../../api';
+import { useNavigate } from 'react-router-dom';
 
-/* const formTemplate = {
+
+const formTemplate = {
     nomeRestaurante: "Pedro",
     telefone: "11951676901",
     estrela: 3,
     site: "https://www.youtubeaaa.com/"
-}; */
-/* 
+};
+
 const formTemplate2 = {
     cep: "",
     numero: "",
     complemento: ""
-}; */
+};
 
 function RestaurantePerfil() {
-    /* const [data, setData] = useState(formTemplate); */
+    const [data, setData] = useState(formTemplate);
 
-    /* const updateFieldHandler = (key, value) => {
+    const navigate = useNavigate();
+
+    const updateFieldHandler = (key, value) => {
         setData((prev) => ({ ...prev, [key]: value }));
-    }; */
+    };
 
-    /*  function atualizar(e) {
-         e.preventDefault();
- 
-         const restauranteInfo = {
-             usuario: 1,
-             id: 1,
-             nomeRestaurante: data.nomeRestaurante,
-             telefone: data.telefone,
-             estrela: data.estrela,
-             site: data.site
-         };
- 
-         api
-             .put(`/restaurantes/${restauranteInfo.id}`, restauranteInfo)
-             .then((res) => {
-                 console.log(res.data);
-                 alert("Atualizado com sucesso!");
-             })
-             .catch((erro) => {
-                 console.error(erro);
-                 alert("Não foi possível atualizar o restaurante, tente novamente.");
-                 navigate("/restaurante-perfil");
-             });
-     } */
+    function atualizar(e) {
+        e.preventDefault();
+
+        const restauranteInfo = {
+            usuario: 1,
+            id: 1,
+            nomeRestaurante: data.nomeRestaurante,
+            telefone: data.telefone,
+            estrela: data.estrela,
+            site: data.site
+        };
+
+        api
+            .put(`/restaurantes/${restauranteInfo.id}`, restauranteInfo)
+            .then((res) => {
+                console.log(res.data);
+                alert("Atualizado com sucesso!");
+            })
+            .catch((erro) => {
+                console.error(erro);
+                alert("Não foi possível atualizar o restaurante, tente novamente.");
+                navigate("/restaurante-perfil");
+            });
+    }
 
     return (
         <>
