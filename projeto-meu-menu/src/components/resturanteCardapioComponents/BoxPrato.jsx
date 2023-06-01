52141383b0a29a72f18bb8141150e7f81d94ea7d
Rafael-Caxixi <rafael.fuzeti@sptech.school>
2023-06-01 10:23:57 -0300

    Deletar prato finalizado

--- projeto-meu-menu/src/components/resturanteCardapioComponents/BoxPrato.jsx
+++ projeto-meu-menu/src/components/resturanteCardapioComponents/BoxPrato.jsx
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from 'react';
 import api from '../../api';
 import ImgPratoCardapio from '../../assets/images/ImgPratoCardapio.png';
 import ImgEditarPrato from '../../assets/images/btnEditarPrato.png';
+import Swal from 'sweetalert2'
 
 function BoxPrato(props) {
     const [cardapio, setCardapio] = useState([]);
     const [pratoSelecionado, setPratoSelecionado] = useState(null);
 
+    const restauranteInfo = {
+        id: sessionStorage.ID_RESTAURANTE_EDICOES
+    }
     useEffect(() => {
         api
-            .get(`/cardapios?id=${1}`)
+            .get(`/cardapios?id=${restauranteInfo.id}`)
             .then((respostaObtida) => {
                 setCardapio(respostaObtida.data);
                 console.log(respostaObtida.data);
@@ -26,6 +30,43 @@ function BoxPrato(props) {
         setPratoSelecionado(pratoId);
         // Outras ações relacionadas à edição do prato...
     };
+
+    /* useEffect(() => {
+        api
+            .delete(`/cardapios?id=${}`)
+            .then((respostaObtida) => {
+                setCardapio(respostaObtida.data);
+                console.log(respostaObtida.data);
+            })
+            .catch((erroObtido) => {
+                console.log(erroObtido);
+            });
+    }, []); */
+
+
+    const deletar = (pratoId) => {
+        api
+            .delete(`/cardapios/${pratoId}`)
+            .then((respostaObtida) => {
+                Swal.fire(
+                    '',
+                    'Prato deletado!',
+                    'success',
+                    {
+                        timer: 2000
+                    }
+                )
+                /* setCardapio(respostaObtida.data); */
+                console.log(respostaObtida.data);
+
+                window.location.reload()
+            })
+            .catch((erroObtido) => {
+                console.log(erroObtido);
+            });
+        // Outras ações relacionadas à edição do prato...
+    };
+
 
     return (
         <>
@@ -44,7 +85,7 @@ function BoxPrato(props) {
                     <div className="div_right_side_pratos">
                         <h4>{item.preco}</h4>
                         <p>{item.estiloGastronomico}</p>
-                        <button id="id_btn_desativar">Deletar</button>
+                        <button id="id_btn_desativar" onClick={() => deletar(item.id)}>Deletar</button>
                         <div className="div_btn_editar_cardapio" onClick={() => handleEditClick(item.id)}>
                             <img src={ImgEditarPrato} alt="" id="id_img_editar_prato" onClick={props.onClick} />
                         </div>
