05201a7b34ad7d27927e6aae91ed300417f449c8
kauan-mendes <kauan.mendes@sptech.school>
2023-05-31 22:17:36 -0300

    Arruma sla o que para o lucas

--- projeto-meu-menu/src/components/restauranteLogadoComponents/ButtonExit.jsx
+++ projeto-meu-menu/src/components/restauranteLogadoComponents/ButtonExit.jsx
@@ -1,48 +1,75 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+
 import { useNavigate } from 'react-router-dom';
+
 import api from '../../api';
 
 function ButtonExit(props) {
-  const navigate = useNavigate();
 
-  const [restaurante, setRestaurante] = useState();
+const navigate = useNavigate();
 
-  const [valido, setValido] = useState();
+const [restaurante, setRestaurante] = useState();
 
-  function botaoMeuRestaurante() {
-    sessionStorage.ID_RESTAURANTE_EDICOES = restaurante.id
-    return (
-      <>
-        <button onClick={() => navigate("/restaurante-perfil")} id='id_button_meu_restaurante'>Meu Restaurante</button>
-      </>
-    );
-  }
+const [valido, setValido] = useState();
 
-  useEffect(() => {
-    api.get(`restaurantes/filtrar/fkUsuario/${sessionStorage.ID_USUARIO}`).then((respostaObtida) => {
-      console.log(respostaObtida.data);
-      setRestaurante(respostaObtida.data);
-      setValido(true);
-      console.log(valido);
-    })
-      .catch((erroObtido) => {
-        setValido(false);
-        console.log(erroObtido);
-      });
-  }, [valido])
+useEffect(() => {
 
-  return (
+api.get(`restaurantes/filtrar/fkUsuario/${sessionStorage.ID_USUARIO}`)
 
-    <>
+.then((respostaObtida) => {
 
-      <div className='box_button_exit'>
-        {props.parametro ? (valido ? botaoMeuRestaurante() : '') : ''}
-        <button onClick={() => navigate("/")} id='id_button_exit'>Sair</button>
-      </div>
+console.log(respostaObtida.data);
 
-    </>
+setRestaurante(respostaObtida.data);
 
-  )
+setValido(true);
+
+console.log(valido);
+
+})
+
+.catch((erroObtido) => {
+
+setValido(false);
+
+console.log(erroObtido);
+
+});
+
+}, [valido]);
+
+function botaoMeuRestaurante() {
+
+sessionStorage.ID_RESTAURANTE_EDICOES = restaurante.id;
+
+return (
+
+<button onClick={() => navigate("/restaurante-perfil")} id='id_button_meu_restaurante'>
+
+Meu Restaurante
+
+</button>
+
+);
+
 }
 
-export default ButtonExit
+return (
+
+<div className='box_button_exit'>
+
+{props.parametro ? (valido && restaurante ? botaoMeuRestaurante() : '') : ''}
+
+<button onClick={() => navigate("/")} id='id_button_exit'>
+
+Sair
+
+</button>
+
+</div>
+
+);
+
+}
+
+export default ButtonExit;
