4e0a14028d810c5ff84168c1db16d8aa1941a354
breno-galante <breno.galante@sptech.school>
2023-06-04 23:36:26 -0300

    Função atualizar foto do cardapio funcionando

--- projeto-meu-menu/src/components/resturanteCardapioComponents/BoxPrato.jsx
+++ projeto-meu-menu/src/components/resturanteCardapioComponents/BoxPrato.jsx
@@ -3,10 +3,13 @@ import api from '../../api';
 import ImgPratoCardapio from '../../assets/images/ImgPratoCardapio.png';
 import ImgEditarPrato from '../../assets/images/btnEditarPrato.png';
 import Swal from 'sweetalert2'
+import UploadFoto from '../UsuarioLogadoComponents/UploadFoto';
 
 function BoxPrato(props) {
     const [cardapio, setCardapio] = useState([]);
     const [pratoSelecionado, setPratoSelecionado] = useState(null);
+    const [selectedFile, setSelectedFile] = useState();
+    
 
     const restauranteInfo = {
         id: sessionStorage.ID_RESTAURANTE_EDICOES
@@ -23,11 +26,12 @@ function BoxPrato(props) {
             });
     }, []);
 
-    const handleEditClick = (pratoId) => {
+    const handleEditClick = (pratoId,arquivo) => {
         sessionStorage.ID_PRATO = pratoId;
-        console.log("hahaha")
-        console.log(sessionStorage.ID_PRATO)
+        console.log("hahaha");
+        console.log(sessionStorage.ID_PRATO);
         setPratoSelecionado(pratoId);
+        setSelectedFile(arquivo)
         // Outras ações relacionadas à edição do prato...
     };
 
@@ -55,6 +59,14 @@ function BoxPrato(props) {
         // Outras ações relacionadas à edição do prato...
     };
 
+    const link = "https://meumenuimagens.blob.core.windows.net/pratos/";
+
+    
+
+
+
+
+
 
     return (
         <>
@@ -64,7 +76,9 @@ function BoxPrato(props) {
                     className={`div_prato_box_cardapio ${pratoSelecionado === item.id ? 'prato-selecionado' : ''}`}
                 >
                     <div className="div_left_side_pratos">
-                        <img src={ImgPratoCardapio} alt="" id="img_prato_cardapio" />
+                        <div className='d-flex justify-content-center align-items-center' style={{ backgroundImage: `url(${link + item.fotoPrato})` }} alt="" id="img_prato_cardapio">
+                            <UploadFoto onFileSelect={props.arquivo} />
+                        </div>
                         <div className="div_nome_descricao_prato">
                             <h3>{item.nome}</h3>
                             <p>{item.descricao}</p>
@@ -74,7 +88,7 @@ function BoxPrato(props) {
                         <h4>{item.preco}</h4>
                         <p>{item.estiloGastronomico}</p>
                         <button id="id_btn_desativar" onClick={() => deletar(item.id)}>Deletar</button>
-                        <div className="div_btn_editar_cardapio" onClick={() => handleEditClick(item.id)}>
+                        <div className="div_btn_editar_cardapio" onClick={() => handleEditClick(item.id, props.arquivo)}>
                             <img src={ImgEditarPrato} alt="" id="id_img_editar_prato" onClick={props.onClick} />
                         </div>
                     </div>
